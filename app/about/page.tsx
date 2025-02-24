6052c8556e7c5d5499126b82e5a7847dcb81a31f
Elxan <elxan.offical.07@gmail.com>
2025-02-24 22:16:01 +0400

    about responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function AboutPage() {
   return (
     <div className="container mb-[80px] flex flex-col gap-5">
-      <div className=" mt-10 flex flex-col items-center justify-center">
+      <div className="mt-10 flex flex-col items-center justify-center">
         <h2 className="text-[24px] text-[#242321] font-medium">About Me.</h2>
         <p className="text-[16px] text-[#666666] font-normal text-center leading-7">
-          My everyday work is presented here, I do what i love, <br />
+          My everyday work is presented here, I do what I love, <br />
           Who I am, what I do and my social networks, connect with me.
         </p>
       </div>
-      <div className="flex gap-7 border border-black">
-        <div className="bg-[#EEEEEE]  pl-10 pr-4 py-11  ">
-          <div className="border overflow-scroll overflow-x-auto  w-[617px] max-h-[500px] border-black">
+      <div className="flex flex-col lg:flex-row md:gap-7">
+        <div className="bg-[#EEEEEE] pl-10 pr-4 lg:pr-10 py-11 overflow-hidden">
+          <div className="overflow-scroll overflow-x-auto lg:w-[620px] h-[500px] border-black">
             <div className="mb-10">
               <h4 className="text-[26px] text-[#242321] font-normal">
                 Danilo Pelzer
@@ -21,13 +21,13 @@ export default function AboutPage() {
                 Chief Executive Officer
               </span>
             </div>
-            <div className="text-[#666666] text-[16px] border border-black mr-4">
+            <div className="text-[#666666] text-[16px] mr-2 lg:mr-4">
               Certainty listening no no behaviour existence assurance situation
               is. Because <br /> add why not esteems amiable him. Interested the
               unaffected mrs law friendship add principles. Indeed on people do
-              merits to. Court heard which up above <br /> hoped grave do. Answer
-              living law things either sir bed length. Looked before <br /> we an on
-              merely. <br />
+              merits to. Court heard which up above <br /> hoped grave do.
+              Answer living law things either sir bed length. Looked before{" "}
+              <br /> we an on merely. <br />
               These no death he at share alone. Yet outward the him compass
               hearted are tedious. <br />
               <br />
@@ -59,7 +59,13 @@ export default function AboutPage() {
           </div>
         </div>
 
-        <div> <img src="/about-me.jpg" alt="" /></div>
+        <div className="flex justify-center md:justify-start mt-5 md:mt-0">
+          <img
+            src="/about-me.jpg"
+            alt="About Me"
+            className="w-full md:w-[300px] lg:w-[500px] xl:w-[500px] rounded-lg"
+          />
+        </div>
       </div>
     </div>
   );
