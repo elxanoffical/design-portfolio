7384e6f5a0d8e7542bc66d46045382626c4068a7
Elxan <elxan.offical.07@gmail.com>
2025-02-25 23:20:20 +0400

    contact

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,71 +2,73 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <div className="container mb-[80px] flex flex-col gap-5">
-      <div className="mt-10 flex flex-col items-center justify-center">
-        <h2 className="text-[24px] text-[#242321] font-medium">About Me.</h2>
-        <p className="text-[16px] text-[#666666] font-normal text-center leading-7">
-          My everyday work is presented here, I do what I love, <br />
-          Who I am, what I do and my social networks, connect with me.
-        </p>
-      </div>
-      <div className="flex flex-col lg:flex-row md:gap-7">
-        <div className="bg-[#EEEEEE] pl-10 pr-4 lg:pr-10 py-11 overflow-hidden">
-          <div className="overflow-scroll overflow-x-auto lg:w-[620px] h-[500px] border-black">
-            <div className="mb-10">
-              <h4 className="text-[26px] text-[#242321] font-normal">
-                Danilo Pelzer
-              </h4>
-              <span className="text-[17px] font-normal">
-                Chief Executive Officer
-              </span>
-            </div>
-            <div className="text-[#666666] text-[16px] mr-2 lg:mr-4">
-              Certainty listening no no behaviour existence assurance situation
-              is. Because <br /> add why not esteems amiable him. Interested the
-              unaffected mrs law friendship add principles. Indeed on people do
-              merits to. Court heard which up above <br /> hoped grave do.
-              Answer living law things either sir bed length. Looked before{" "}
-              <br /> we an on merely. <br />
-              These no death he at share alone. Yet outward the him compass
-              hearted are tedious. <br />
-              <br />
-              Listening no no behaviour existence assurance situation is.
-              Because add why not esteems amiable him. Interested the unaffected
-              mrs law friendship add principles. Indeed on people do merits to.
-              Court heard which up above hoped grave do. Answer living law
-              things either sir bed length. Looked before we an on merely.{" "}
-              <br />
-              These no death he at share alone. Yet outward the him compass
-              hearted are tedious. <br />
-              <br />
-              Because add why not esteems amiable him. Interested the unaffected
-              mrs law friendship add principles. Indeed on people do merits to.
-              Court heard which up above hoped grave do. Answer living law
-              things either sir bed length. Looked before we an on merely.{" "}
-              <br />
-              <br />
-              Certainty listening no no behaviour existence assurance situation
-              is. Because add why not esteems amiable him. Interested the
-              unaffected mrs law friendship add principles. Indeed on people do
-              merits to. Court heard which up above hoped grave do. Answer
-              living law things either sir bed length. Looked before we an on
-              merely. <br />
-              These no death he at share alone. Yet outward the him compass
-              hearted are tedious.
-              <br />
+    <section className="">
+      <div className="container mb-[80px] flex flex-col gap-5">
+        <div className="mt-10 flex flex-col items-center justify-center">
+          <h2 className="text-[24px] text-[#242321] font-medium">About Me.</h2>
+          <p className="text-[16px] text-[#666666] font-normal text-center leading-7">
+            My everyday work is presented here, I do what I love, <br />
+            Who I am, what I do and my social networks, connect with me.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row md:gap-7">
+          <div className="bg-[#EEEEEE] pl-10 pr-4 lg:pr-10 py-11 overflow-hidden">
+            <div className="overflow-scroll overflow-x-auto lg:w-[620px] h-[500px] border-black">
+              <div className="mb-10">
+                <h4 className="text-[26px] text-[#242321] font-normal">
+                  Danilo Pelzer
+                </h4>
+                <span className="text-[17px] font-normal">
+                  Chief Executive Officer
+                </span>
+              </div>
+              <div className="text-[#666666] text-[16px] mr-2 lg:mr-4">
+                Certainty listening no no behaviour existence assurance
+                situation is. Because <br /> add why not esteems amiable him.
+                Interested the unaffected mrs law friendship add principles.
+                Indeed on people do merits to. Court heard which up above <br />{" "}
+                hoped grave do. Answer living law things either sir bed length.
+                Looked before <br /> we an on merely. <br />
+                These no death he at share alone. Yet outward the him compass
+                hearted are tedious. <br />
+                <br />
+                Listening no no behaviour existence assurance situation is.
+                Because add why not esteems amiable him. Interested the
+                unaffected mrs law friendship add principles. Indeed on people
+                do merits to. Court heard which up above hoped grave do. Answer
+                living law things either sir bed length. Looked before we an on
+                merely. <br />
+                These no death he at share alone. Yet outward the him compass
+                hearted are tedious. <br />
+                <br />
+                Because add why not esteems amiable him. Interested the
+                unaffected mrs law friendship add principles. Indeed on people
+                do merits to. Court heard which up above hoped grave do. Answer
+                living law things either sir bed length. Looked before we an on
+                merely. <br />
+                <br />
+                Certainty listening no no behaviour existence assurance
+                situation is. Because add why not esteems amiable him.
+                Interested the unaffected mrs law friendship add principles.
+                Indeed on people do merits to. Court heard which up above hoped
+                grave do. Answer living law things either sir bed length. Looked
+                before we an on merely. <br />
+                These no death he at share alone. Yet outward the him compass
+                hearted are tedious.
+                <br />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex justify-center md:justify-start mt-5 md:mt-0">
-          <img
-            src="/about-me.jpg"
-            alt="About Me"
-            className="w-full md:w-[300px] lg:w-[500px] xl:w-[500px] rounded-lg"
-          />
+          <div className="flex justify-center md:justify-start mt-5 md:mt-0">
+            <img
+              src="/about-me.jpg"
+              alt="About Me"
+              className="w-full md:w-[300px] lg:w-[500px] xl:w-[500px] rounded-lg"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
---
+++ app/contact/page.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <section>
+      <div className=" border container flex flex-col gap-[60px]">
+        <img src="/contact.jpg" alt="" />
+        <div className="border border-black flex gap-[150px]">
+          <div className="flex flex-col gap-[50px]">
+            <div>
+              <h3 className="text-[26px] font-medium">Contact Me.</h3>
+              <p className="text-[16px] text-[#666666] font-normal">
+                Write me an e-mail via the form, <br /> or just send us an
+                e-mail directly at
+              </p>
+              <span className="text-[16px] text-[#E09A0E]">
+                danilo@pelzer.com
+              </span>
+            </div>
+            <div>
+              <h4 className="text-[16px] text-[#666666] font-semibold mb-4">
+                Telephone:
+              </h4>
+              <p className="text-[16px] text-[#666666] font-normal">
+                1-408-555-5555
+              </p>
+              <p className="text-[16px] text-[#666666] font-normal">
+                1-408-555-2259
+              </p>
+            </div>
+          </div>
+            <form className="border border-black w-[783px] flex flex-col gap-10" action="">
+              <div className="flex items-center gap-7">
+                <div className="flex items-center border-b  w-[50%]">
+                  <label htmlFor="">Name.</label>
+                  <input className=" outline-none" type="text" />
+                </div>
+                <div className="flex items-center border-b  w-[50%]">
+                  <label htmlFor="">Email.</label>
+                  <input className=" outline-none" type="text" />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="">Message.</label>
+                <input className="border outline-none" type="textarea" />
+              </div>
+            </form>
+        </div>
+      </div>
+    </section>
+  );
+}
