fc2251cfc9be87576df8781328b2e7918d143535
Elxan <elxan.offical.07@gmail.com>
2025-02-26 22:11:30 +0400

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function ContactPage() {
   return (
     <section>
-      <div className=" border container flex flex-col gap-[60px]">
+      <div className="  container flex flex-col gap-[60px]">
         <img src="/contact.jpg" alt="" />
-        <div className="border border-black flex gap-[150px]">
+        <div className=" flex gap-[150px]">
           <div className="flex flex-col gap-[50px]">
             <div>
               <h3 className="text-[26px] font-medium">Contact Me.</h3>
@@ -13,8 +13,9 @@ export default function ContactPage() {
                 Write me an e-mail via the form, <br /> or just send us an
                 e-mail directly at
               </p>
-              <span className="text-[16px] text-[#E09A0E]">
+              <span className="text-[16px] text-[#E09A0E] group cursor-pointer">
                 danilo@pelzer.com
+                <div className="bg-[#c08c23] h-[1px] w-0 group-hover:w-[55%] transition-all duration-200"></div>
               </span>
             </div>
             <div>
@@ -29,22 +30,45 @@ export default function ContactPage() {
               </p>
             </div>
           </div>
-          <form
-            className="border border-black w-[783px] flex flex-col gap-10"
-            action=""
-          >
+
+          <form className=" w-[783px] flex flex-col gap-10" action="">
             <div className="flex items-center gap-7">
-              <div className="flex items-center border-b  w-[50%]">
-                <label className="text-[16px] text-[#666666]" htmlFor="">
-                  Name.
-                </label>
-                <input className=" outline-none" type="text" />
+              <div className="flex flex-col w-[50%] group relative">
+                <div className="flex mb-2">
+                  <label
+                    className="text-[16px] text-[#666666] w-[60px]"
+                    htmlFor="inputId"
+                  >
+                    Name.
+                  </label>
+                  <input
+                    id="inputId"
+                    className="outline-none w-full"
+                    type="text"
+                  />
+                </div>
+                <div className="bg-neutral-200 h-[1px] w-full relative">
+                  <div className="absolute bottom-0 left-0 h-[1px] bg-yellow-500 w-[60px] transition-all duration-300 group-focus-within:w-full"></div>
+                </div>
               </div>
-              <div className="flex items-center border-b  w-[50%]">
-                <label className="text-[16px] text-[#666666]" htmlFor="">
-                  Email.
-                </label>
-                <input className=" outline-none" type="text" />
+
+              <div className="flex flex-col w-[50%] group relative">
+                <div className="flex mb-2">
+                  <label
+                    className="text-[16px] text-[#666666] w-[60px]"
+                    htmlFor="inputId"
+                  >
+                    Email.
+                  </label>
+                  <input
+                    id="inputId"
+                    className="outline-none w-full"
+                    type="text"
+                  />
+                </div>
+                <div className="bg-neutral-200 h-[1px] w-full relative">
+                  <div className="absolute bottom-0 left-0 h-[1px] bg-yellow-500 w-[60px] transition-all duration-300 group-focus-within:w-full"></div>
+                </div>
               </div>
             </div>
             <div>
@@ -52,10 +76,9 @@ export default function ContactPage() {
                 Message.
               </label>
               <textarea
-              rows={5}
+                rows={5}
                 name="textarea"
-                className="w-full  border-b-2 resize-none outline-none"
-                placeholder="Message..."
+                className="w-full  border-b-2 resize outline-none"
               ></textarea>
             </div>
           </form>
